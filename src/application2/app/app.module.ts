fbb4c7c7f64dc02b828f517ec94d730d6a53056c
Vamsee Kumar Vella <vamsee@olivetech.net>
2018-04-10 14:15:14 +0530

    first commit

---
+++ src/application2/app/app.module.ts
@@ -0,0 +1,20 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {LibModule} from '../../shared/module';
+
+import {AppComponent} from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    LibModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
